e4c24f11714b44da27ffd7c731452458728b5a38
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-10-01 10:39:26 -0500

    added eval command

---
+++ commands/eval.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'eval',
+  description: 'Evaluates code following command in javascript',
+  execute(message, args) {
+    let code = args.slice(1, args.length).join(' ');
+    try {
+      message.channel.send(eval(code), { code: 'javascript' });
+    } catch (err) {
+      this.channel.send(`ERROR!\n${err}`, { code: 'javascript' });
+    }
+  }
+};
