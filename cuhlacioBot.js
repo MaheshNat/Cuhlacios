2ba2cc93654ea1fba497d8b65f953a2be1649ce1
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-10-06 16:23:44 -0500

    removed chron job from cuhlacioBot.js file, using Scheduler.js

--- cuhlacioBot.js
+++ cuhlacioBot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const cron = require('cron');
 require('dotenv').config();
 
 const client = new Discord.Client();
@@ -7,35 +6,6 @@ client.commands = new Discord.Collection();
 client.mongoose = require('./utils/mongoose');
 client.config = require('./config.js');
 client.loader = require('./modules/Loader');
-
-// Consider moving cron.job to a module file or functions.js file. The bot.js / index.js should be a small as possible.
-cron
-  .job(
-    client.config.clearSchedule,
-    () => {
-      console.log('executing');
-      const server = client.guilds.cache.get(client.config.guildID);
-      const channels = server.channels.cache;
-      channels.forEach((channel, key, map) => {
-        if (channel instanceof Discord.TextChannel) {
-          if (
-            channel.name === 'join-log' ||
-            channel.name === 'announcements' ||
-            channel.name === 'computer-science' ||
-            channel.name === 'memes'
-          ) {
-            return;
-          }
-          client.commands.get('clear').clear(channel);
-          console.log(`Cleared channel '${channel.name}'`);
-        }
-      });
-    },
-    undefined,
-    true,
-    'America/Chicago'
-  )
-  .start();
 
 const init = async () => {
   console.clear();
@@ -50,6 +20,7 @@ const init = async () => {
     await client.logger.warn('URI needs to be defined for mongoose.');
   }
   await client.login(process.env.token);
+  client.scheduler.start(client);
 };
 
 init();
