eeea7e33efc19f8d1c32c4832877bf721e974db2
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-10-09 00:11:28 -0500

    modified roast command to used cached .json file instead of database

--- commands/roast.js
+++ commands/roast.js
@@ -1,17 +1,28 @@
 const axios = require('axios');
-const Roast = require('../models/roast.js');
+const fs = require('fs');
 
 module.exports = {
   name: 'roast',
   description: 'Roasts the shit out of you.',
-  execute(message, args) {
-    Roast.countDocuments().then(count => {
-      const random = Math.floor(Math.random() * count);
-      Roast.findOne()
-        .skip(random)
-        .then(roast => {
-          message.channel.send(roast.roast.substring(0, 2000));
-        });
-    });
+  async execute(message, args) {
+    let roasts;
+    if (fs.existsSync('./utils/roasts.json')) {
+      roasts = JSON.parse(fs.readFileSync('./utils/roasts.json'));
+    } else {
+      const res = await axios.get(
+        'https://zenodo.org/api/files/4b018c73-e22f-4c7c-bd0e-bdabdbd9a28b/macro-norm-violations-n10-t0-misogynistic-slurs.csv'
+      );
+      roasts = res.data.split('\n')
+
+      fs.writeFile(
+        './utils/roasts.json',
+        JSON.stringify(roasts),
+        err => {
+          if (err) return console.log(err);
+          console.log('roasts.json file written.');
+        }
+      );
+    }
+    message.channel.send(roasts[Math.floor(Math.random() * roasts.length)])
   }
 };
--- models/roast.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const roastSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  roast: String
-});
-module.exports = mongoose.model('Roast', roastSchema);
