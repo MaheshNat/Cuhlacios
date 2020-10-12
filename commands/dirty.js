5981b0452dcef24c31ac2b5e8535eb5c61c78339
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-10-12 15:35:46 -0500

    limited the dirty command to nsfw channels

--- commands/dirty.js
+++ commands/dirty.js
@@ -4,6 +4,9 @@ module.exports = {
   name: 'dirty',
   description: 'Sends a random dirty emoji image',
   execute(message, args) {
+    if (!message.channel.nsfw) {
+      return message.reply('This command can only be used in an nsfw channel.');
+    }
     message.channel.send(
       dirtyEmojiImages[Math.floor(Math.random() * dirtyEmojiImages.length)]
     );
