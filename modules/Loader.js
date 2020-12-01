e618fd7512e15dcc81ea2d163014b49a601a21b2
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-12-01 16:34:52 -0600

    debugging heroku check nvidia

--- modules/Loader.js
+++ modules/Loader.js
@@ -59,7 +59,9 @@ exports.registerRestrictedCommands = client => {
 };
 
 exports.checkNvidiaDrop = client => {
+  console.log('reached here called func');
   client.interval = setInterval(() => {
+    console.log('reached here');
     axios
       .get(
         'https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402'
