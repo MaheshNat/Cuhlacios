8bd525dfc4a2cbd917a7199d1b471b47c7ec424f
MaheshNat <maheshkumar.natamai@gmail.com>
2021-02-14 18:59:57 -0600

    update user route

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,11 @@ const axios = require('axios');
 
 router.get('/:name', async (req, res) => {
   try {
-    await axios.get(
-      `https://www.etoro.com/api/streams/v2/streams/user-trades/${req.params.name}`
+    const { data } = await axios.get(
+      `https://www.etoro.com/api/logininfo/v1.1/users/${req.params.name}`
     );
-    res.status(200).json({ exists: true });
+    if (data.optOut) res.status(200).json({ private: true });
+    else res.status(200).json({ exists: true });
   } catch (e) {
     res.status(200).json({ exists: false });
   }
